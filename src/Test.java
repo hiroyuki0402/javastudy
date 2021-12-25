1487ffbaee4a4fcbd1c9ee82f6191e6c964f4d3e
hiroyuki0402 <88645049+hiroyuki0402@users.noreply.github.com>
2021-12-25 10:06:01 +0900

    [clean] リファクタリング

--- src/Test.java
+++ src/Test.java
@@ -1,118 +1,170 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Test {
 
 	private static Scanner scanner;
-	private static int comHp = 0;
+	private static int computerHp = 0;
 	private static int playerHp = 0;
-	private static String pName = "";
-	private static String cName = "JAVA";
+	private static String playerName = "";
+	private static String computerName = "";
+	private static Properties[] properties = new Properties[2];
 
 	public static void main(String[] args) {
-		nameAndHitPoints(scanner(IsType.String));
-		battle();
-
+		batleSetUp();
+		isBattle();
 	}
 
-	public static Object scanner(IsType isType) {
+	// MARK: METHODS
+
+	/// システム入力
+	/// - Parameters: enum FormatBrunch
+	/// - param: 型の指定
+	/// - Returns: オブジェクト
+	public static Object scanner(FormatBrunch formart) {
+		// FIXME: ジェネリックスの方が良いかも
 		scanner = new Scanner(System.in);
-		switch (isType) {
+		switch (formart) {
 		case Int:
 			return scanner.nextInt();
-		default:
+		case String:
 			return scanner.next();
-		}
-	}
-
-	static void nameAndHitPoints(Object name) {
-		comHp = randomNumber(ParameterBranch.HITPOINT);
-		playerHp = randomNumber(ParameterBranch.HITPOINT);
-		pName = (String)name;
-		System.out.println("挑戦者は" + name + "さん");
-		System.out.println(name + "さんのHPは" + playerHp + "です");
-		System.out.println("対戦相手は" + cName +"さん");
-		System.out.println(cName + "さんのHPは" + comHp + "です");
-	}
-
-	static int randomNumber(ParameterBranch parameterBranch) {
-		Random random = new Random();
-		switch (parameterBranch) {
-		case HITPOINT:
-			return random.nextInt(10) + 5;
-		case DAMAGE:
-			return random.nextInt(1) + 5;
-		case COMPUTER:
-			return random.nextInt(1) + 2;
 		default:
-			return random.nextInt(0);
+			return "err";
 		}
 	}
 
-	static void hitPoint() {
+	/// システム入力
+	/// - Parameters: enum ParameterBranch
+	/// - param: 用途の指定
+	/// - Returns: ランダムな数字
+	static int randomNumber(ParameterBranch parameterBranch) {
+		switch (parameterBranch) {
+		case DAMAGE:
+			return (int) (Math.random() * 3) + 5;
+		case COMPUTERHUND:
+			return (int) (Math.random() * 3);
+		default:
+			return (int) Math.random() * 0;
+		}
 	}
 
-	static void battle() {
-		for (;;) {
-			var choiceNumber = (int) scanner(IsType.Int);
-			var computerHand = randomNumber(ParameterBranch.COMPUTER);
-			var randomNumber = randomNumber(ParameterBranch.HITPOINT);
-			if (playerHp <= 0 || comHp <= 0) {
-				isChecked();
-			} else {
-				if (choiceNumber == computerHand) {
-					continue;
+	/// バトルの準備
+	/// - Parameters:
+	/// - Returns: -
+	static void batleSetUp() {
+		// FIXME: java構造体できるのか問題
+		System.out.println("名前を入力してください");
+		properties[Member.player.ordinal()] = new Properties(scanner(FormatBrunch.String).toString(), 15);
+		properties[Member.computer.ordinal()] = new Properties("JAVA", 15);
+		playerHp = properties[Member.player.ordinal()].getHitpoint();
+		computerHp = properties[Member.computer.ordinal()].getHitpoint();
+		playerName = properties[Member.player.ordinal()].getName();
+		computerName = properties[Member.computer.ordinal()].getName();
+	}
 
-				} else if (choiceNumber == 1 && computerHand == 2) {
-					System.out.println(pName + "さんは:" + choiceNumber);
-					System.out.println(cName + "さんは:" + computerHand);
-					comHp -= 1 + randomNumber;
-					System.out.println(cName + ":" + comHp);
-					System.out.println(pName + ":" + playerHp);
-					isChecked();
-					continue;
-				} else if (choiceNumber == 2 && computerHand == 3) {
-					comHp -= 1 + randomNumber;
-					System.out.println(pName + "さんは:" + choiceNumber);
-					System.out.println(cName + "さんは:" + computerHand);
-					System.out.println(cName + ":" + comHp);
-					System.out.println(pName + ":" + playerHp);
-					isChecked();
-					continue;
-				} else if (choiceNumber == 3 && computerHand == 1) {
-					playerHp -= 1 + randomNumber;
-					System.out.println(pName + "さんは:" + choiceNumber);
-					System.out.println(cName + "さんは:" + computerHand);
-					System.out.println(cName + ":" + comHp);
-					System.out.println(pName + ":" + playerHp);
-					isChecked();
+	/// バトル
+	/// - Parameters:
+	/// - Returns: -
+	static void isBattle() {
+		System.out.println("最初はぐー");
+
+		for (;;) {
+			if (playerHp > 0 && computerHp > 0) {
+				System.out.println("じゃんけん");
+				System.out.println("グー:0\nチョキ:1\nパー:2");
+				int computerHand = randomNumber(ParameterBranch.COMPUTERHUND);
+				int playerHand = (int) scanner(FormatBrunch.Int);
+				if (playerHand >= 3) {
+					System.out.println("入力数字を確認してください: 入力可能数字0~2");
 					continue;
 				}
-				isChecked();
+				System.out.println("ポイ");
+				System.out.println(computerHand);
+				battleIsChecked(playerHand, computerHand);
+			} else {
+				WinOrLoseChecked();
 				break;
 			}
 		}
 	}
 
-	static void isChecked() {
-		if (playerHp <= 0) {
-			System.out.println(cName + ":" + comHp);
-			System.out.println(pName + ":" + playerHp);
-			System.out.println("勝者:" + pName);
-		} else if (comHp <= 0) {
-			System.out.println(cName + ":" + comHp);
-			System.out.println(pName + ":" + playerHp);
-			System.out.println("勝者:" + cName);
-		}
+	/// バトルのチェック
+	/// - Parameters:
+	/// - paramA: prayerの選択したじゃんけんの数字
+	/// - paramB: Computerの選択したじゃんけんの数字
+	/// - Returns: 戻り値の説明
+	static void battleIsChecked(int player, int computer) {
+		// FIXME: きたない・冗長すぎ
+		var damage = randomNumber(ParameterBranch.DAMAGE);
 
+		var check = (player - computer + 3) % 3;
+		switch (check) {
+		case 0:
+			System.out.println("アイコ");
+			break;
+		case 2:
+			System.out.println(playerName + "さんは:" + player);
+			System.out.println(computerName + "さんは:" + computer);
+			System.out.println(playerName + "の勝ち");
+			System.out.println(computerName + "さんに" + (1 + damage) + "ダメージを与えた");
+			computerHp -= 1 + damage;
+			System.out.println(playerName + "さんのHP:" + playerHp);
+			System.out.println(computerName + "さんのHP:" + computerHp);
+			break;
+		default:
+			System.out.println(playerName + "さんは:" + player);
+			System.out.println(computerName + "さんは:" + computer);
+			System.out.println(computerName + "の勝ち");
+			System.out.println(playerName + "さんに" + (1 + damage) + "ダメージを与えた");
+			playerHp -= 1 + damage;
+			System.out.println(playerName + "さんのHP:" + playerHp);
+			System.out.println(computerName + "さんのHP:" + computerHp);
+			break;
+		}
 	}
 
+	/// 勝敗の判定
+	/// - Parameters:
+	/// - Returns: -
+	static void WinOrLoseChecked() {
+		// TODO: 再実施処理
+		if (playerHp < 0) {
+			System.out.println("勝負あり!!勝者:" + computerName);
+		} else if (computerHp < 0) {
+			System.out.println("勝負あり!!勝者:" + playerName);
+		}
+	}
+	
+	// MARK: ENUMS
+
 	public enum ParameterBranch {
-		HITPOINT, DAMAGE, COMPUTER
+		HITPOINT, DAMAGE, COMPUTERHUND
 	};
 
-	public enum IsType {
-		Int, String,
+	public enum Member {
+		player, computer
 	};
 
+	public enum FormatBrunch {
+		Int, String
+	}
+
+	public enum Hand {
+		Rock, paper, scissors
+	}
+}
+
+class Properties {
+	private String name;
+	private int hitpoint;
+	public Properties(String name, int hitpoint) {
+		this.name = name;
+		this.hitpoint = hitpoint;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getHitpoint() {
+		return hitpoint;
+	}
 };
